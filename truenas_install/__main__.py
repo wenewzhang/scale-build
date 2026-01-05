c6b30a37c0e6070847c0f7169330d41e0d3c7ed2
Wenew Zhang <wenewboy@gmail.com>
2026-01-05 12:27:25 +0800

    fix: use sh -c to properly set PATH for update-grub in chroot

--- truenas_install/__main__.py
+++ truenas_install/__main__.py
@@ -593,7 +593,7 @@ def main():
                 #         cp.returncode, f'Failed to execute truenas-initrd: {cp.stderr}'
                 #     )
                 write_progress(0.9, "Updating GRUB")
-                run_command(["chroot", root, "update-grub"], env={"PATH": "/usr/sbin:/usr/bin:/sbin:/bin"})
+                run_command(["chroot", root, "sh", "-c", "PATH=/usr/sbin:/usr/bin:/sbin:/bin update-grub"])
                 write_progress(0.91, "after update-grub ")
                 logger.info("after 91...")
                 # We would like to configure fips bit as well here
