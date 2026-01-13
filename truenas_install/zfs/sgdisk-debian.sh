499cea4a6e88e60afd3f30b3054f6546411bb2ea
Wenew Zhang <wenewboy@gmail.com>
2026-01-13 10:48:58 +0800

    fix:mbr prompt

--- truenas_install/zfs/sgdisk-debian.sh
+++ truenas_install/zfs/sgdisk-debian.sh
@@ -7,6 +7,10 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+DISK="$1"
+
+swapoff --all
+
 # --- 1. Disk preparation and partitioning (based on image_f21ae8.jpg) ---
 udevadm settle
 
@@ -23,14 +27,16 @@ sgdisk -n2:0:+524288K -t2:EF00 "$DISK"
 sgdisk -n3:0:+1G -t3:BF01 "$DISK"
 sgdisk -n4:0:0 -t4:BF00 "$DISK"
 # Enable the 'pmbr_boot' flag for BIOS compatibility with GPT
-if [[ "$boot_mode" =~ ^[Yy]$ ]]; then
-    echo "→ Enabling Protective MBR boot flag for BIOS compatibility(legacy BIOS boot, NOT UEFI)..."
+read -p "Enabling MBR boot flag for BIOS compatibility(legacy BIOS boot, NOT UEFI). (y/N): " -r confirm
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    echo "legacy BIOS boot, NOT UEFI"
     parted -s "$DISK" disk_set pmbr_boot on
 else
-    echo "→ Skipping pmbr_boot (assuming UEFI boot)."
+    echo "Skipping pmbr_boot (assuming UEFI boot)."
 fi
 
 echo " Partitioning complete on: $DISK"
 echo "  Partition 1: BIOS Boot (EF02)"
 echo "  Partition 2: EFI System Partition (EF00, 512 MiB)"
-echo "  Partition 3: ZFS (BF01, remaining space)"
+echo "  Partition 3: ZFS Boot Pool (BF01, 1024 MiB)"
+echo "  Partition 4: ZFS Root Pool (BF00, remaining space)"
