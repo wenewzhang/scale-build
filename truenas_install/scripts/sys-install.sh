169a18020a0c119a88fd6f482ef75b9aca31f9d9
Wenew Zhang <wenewboy@gmail.com>
2026-01-14 15:50:29 +0800

    make dir quiet

--- truenas_install/scripts/sys-install.sh
+++ truenas_install/scripts/sys-install.sh
@@ -2,5 +2,5 @@
 mkdir /tmp/rootfs
 mount -t squashfs /cdrom/TrueNAS-SCALE.update /tmp/rootfs
 unsquashfs -d /mnt -f -da 16 -fr 16  /tmp/rootfs/rootfs.squashfs
-mkdir /mnt/etc/zfs
+mkdir -p /mnt/etc/zfs
 cp /etc/zfs/zpool.cache /mnt/etc/zfs/
