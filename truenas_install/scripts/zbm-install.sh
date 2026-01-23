76c3d441ff6917afcf8463d83c25f2a6b0bf28b6
Wenew Zhang <wenewboy@gmail.com>
2026-01-23 09:54:16 +0800

    zbm: create & export & grub-sgdisk

--- truenas_install/scripts/zbm-install.sh
+++ truenas_install/scripts/zbm-install.sh
@@ -15,14 +15,16 @@ STEP="$2"
 if [ -z "$STEP" ]; then
     echo "Usage: $0 [1-7] [additional arguments for step 6]"
     echo "Steps:"
-    echo "  1: zgenhostid"
+    echo "  1: sgdisk zgenhostid"
     echo "  2: sgdisk"
     echo "  3: create zroot"
-    echo "  4: mount show"
-    echo "  5: Update Initramfs (all kernels)"
-    echo "  6: Install GRUB to specific disk (Requires 2 arguments: e.g.,<6> <disk>)"
-    echo "  7: Update GRUB configuration"
-    echo "  8: Updating /etc/fstab..."
+    echo "  4: create dataset"
+    echo "  5: mount show"
+    echo "  6: chroot into mounted system"
+    echo "  7: zpool export/import and mount datasets"
+    echo "  8: GRUB-style disk partitioning (./sgdisk-debian.sh)"
+    echo "  9: Update GRUB configuration"
+    echo " 10: Updating /etc/fstab..."
     exit 1
 fi
 
@@ -39,18 +41,19 @@ POOL_DEVICE="${POOL_DISK}${POOL_PART}"
 
 ID="zuti2601"
 
-zpool labelclear -f "$POOL_DISK"
 
-# wipefs -a "$POOL_DISK"
-wipefs -a "$BOOT_DISK"
-
-# sgdisk --zap-all "$POOL_DISK"
-sgdisk --zap-all "$BOOT_DISK"
 
 case $STEP in
     1)
-        echo ">>> [Step 1] zgenhostid..."
+        echo ">>> [Step 1]  sgdisk zgenhostid..."
         zgenhostid -f 0x00bab10c
+        zpool labelclear -f "$POOL_DISK"
+
+        # wipefs -a "$POOL_DISK"
+        wipefs -a "$BOOT_DISK"
+
+        # sgdisk --zap-all "$POOL_DISK"
+        sgdisk --zap-all "$BOOT_DISK"
         ;;
     2)
         echo ">>> [Step 2] sgdisk..."
@@ -67,23 +70,40 @@ case $STEP in
         -O relatime=on \
         -o autotrim=on \
         -o compatibility=openzfs-2.2-linux \
-        -m none zroot "$POOL_DEVICE"
-
+        -m none zroot "$POOL_DEVICE"    
+        ;;  
+    4)
+        echo ">>> [Step 4] create dataset"
         zfs create -o mountpoint=none zroot/ROOT
         zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/${ID}
         zfs create -o mountpoint=/home zroot/home
         zpool set bootfs=zroot/ROOT/${ID} zroot  
-        zpool export zroot
-
-        zpool import -N -R /mnt zroot
-        zfs mount zroot/ROOT/${ID}
-        zfs mount zroot/home      
         ;;  
-    4)
-        echo ">>> [Step 4] udevadm"
+    5)
+        echo ">>> [Step 4] mount show"
         udevadm trigger
         mount | grep mnt
+        zfs list
         ;;      
+    6)
+        echo ">>> [Step 6] chroot into mounted system..."
+        mount -t proc proc /mnt/proc
+        mount -t sysfs sys /mnt/sys
+        mount -B /dev /mnt/dev
+        mount -t devpts pts /mnt/dev/pts
+        chroot /mnt /bin/bash
+        ;;
+    7)
+        echo ">>> [Step 7] zpool export/import and mount datasets..."
+        zpool export zroot
+        zpool import -N -R /mnt zroot
+        zfs mount zroot/ROOT/${ID}
+        zfs mount zroot/home
+        ;;
+    8)
+        echo ">>> [Step 8] GRUB-style disk partitioning..."
+        ./sgdisk-debian.sh $BOOT_DISK
+        ;;
     *)
         echo "others"
         ;;
